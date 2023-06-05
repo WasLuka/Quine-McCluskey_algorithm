783f8d6ea22cfa27a11f10b1d9cbff308f7cea4d
WasLuka <104713428+WasLuka@users.noreply.github.com>
2023-06-05 18:31:59 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -26,6 +26,7 @@ int main()
 {   
     int cache;
     int count;
+    char buffer[4] = {0,0,0,0};
 
     std::cin >> cache;
     const int numSpremenljivke = cache;//koliko spremenljivk imamo
@@ -52,10 +53,6 @@ int main()
         }
     }
     
-    for(int i = 0;i < numMintermi;i++)
-    {
-        std::cout << mintermi[i] << "\n";
-    }
 
     /*
     2 je tam zato da imam "dve tabeli" med katerima preklapljamo med poenostavljanjem
@@ -63,7 +60,7 @@ int main()
     numMintermi je za specifičen minterm kjer je njegova vrednost shranjena
     numSpremenljivke je za specifičen bit v mintermu ki je lahko shranjen tudi kot -
     */
-    char mintermichar[2][numSpremenljivke+1][numMintermi][numSpremenljivke] = {'.'};
+    char mintermichar[2][numSpremenljivke+1][numMintermi][numSpremenljivke];
     /*
     Najprej se ga razdeli na dva dela za dve tabeli potem po številu "1" v posameznem mintermu,
     potem na število mintermov, in spet na število spremenljivk da lahko shranimo ta minterm
@@ -79,6 +76,7 @@ int main()
             for(int j = 0;j < numSpremenljivke;j++)
             {
                 mintermichar[0][k][i][j] = '.';
+                mintermichar[1][k][i][j] = '.';
             }
 
             if(stBitovEna(mintermi[i]) == k)
@@ -86,7 +84,7 @@ int main()
                 for(int j = 0;j < numSpremenljivke;j++)
                 {
                     cache = mintermi[i];
-                    mintermichar[0][k][count][j] = bitI(cache, j);
+                    mintermichar[0][k][count][numSpremenljivke-j-1] = bitI(cache, j);
                 }
                 count++;
             }
@@ -108,50 +106,116 @@ int main()
         }
         std::cout << "\n" << std::endl;
     }
+    std::cout << "_____________________\n";
 
     //tukaj naprej se začne minimizacija
 
-    int cikel = 0;
-    int zadnjaTabela;
-    int novaTabela;
-
+    bool end;
     int enakost;
     char primernik;
     char primerjeni;
+    int mintermadd;
+    int check;
     while(1)
     {   
-        zadnjaTabela = (cikel%2 == 0 ? 1 : 0);
-        novaTabela = (cikel%2 == 1 ? 1 : 0);
-
-        for(int nivo = 0;nivo < numSpremenljivke-cikel;nivo++)
-        {
-            for(int minterm = 0;minterm < numMintermi;minterm++)
+        end = true;
+        for(int nivo = 0;nivo < numSpremenljivke;nivo++)//st "1"
+        {   
+            mintermadd = 0;
+            for(int minterm = 0;minterm < numMintermi;minterm++)//mintermi z tem st "1"
             {   
-                if(mintermichar[zadnjaTabela][nivo][minterm][0] == '.')
-                {
+                if(mintermichar[0][nivo][minterm][0] == '.')
+                {   
                     goto preskoci;
                 }
                 //dva for loopa ki gresta iz po št. "1" v mintermu in čez vse posamične minterme
-                for(int minterm2 = 0;minterm2 < numMintermi;minterm2++)
-                {
-                    for(int m = 0;m < numSpremenljivke;m++)
+                for(int minterm2 = 0;minterm2 < numMintermi;minterm2++)//mintermi z +1 st "1" za dajanje skupaj
+                {   
+                    enakost = 0;
+                    if(mintermichar[0][nivo+1][minterm2][0] == '.')
                     {
-                        primernik = mintermichar[zadnjaTabela][nivo][minterm][m];
-                        primerjeni = mintermichar[zadnjaTabela][nivo+1][minterm2][m];
-                        if(!primernik == primerjeni)
+                        goto praskoci;
+                    }
+                    for(int m = 0;m < numSpremenljivke;m++)//ce so enaki razen enga
+                    {
+                        primernik = mintermichar[0][nivo][minterm][m];
+                        primerjeni = mintermichar[0][nivo+1][minterm2][m];
+                        if(primernik == primerjeni)
                         {
                             enakost++;
                         }
                     }
-                    if(enakost == 1)
-                    {
-                        //tukaj najdeš razliko in jo vpišeš v novo tabelo
+                    if(enakost == (numSpremenljivke-1))
+                    {   
+                        end = false;
+                        check = 0;
+                        for(int m = 0;m < numSpremenljivke;m++)//primerjanje shranimo v buffer
+                        {
+                            primernik = mintermichar[0][nivo][minterm][m];
+                            primerjeni = mintermichar[0][nivo+1][minterm2][m];
+                            if(primernik == primerjeni)
+                            {
+                                buffer[m] = primernik;
+                            }
+                            else
+                            {
+                                buffer[m] = '-';
+                            }
+                        
+                        }
+                        for(int mintermC = 0;mintermC < numMintermi;minterm++)//tukaj preverimo ce je ta kombinacija ze vpisana
+                        {
+                            for(int m = 0;m < numSpremenljivke;m++)
+                            {
+                                if(buffer[m] == mintermichar[1][nivo][mintermC][m])
+                                {
+                                    check++;
+                                }
+                            }
+                            if(check == numSpremenljivke)
+                            {
+                                for(int m = 0;m < numSpremenljivke;m++)
+                                {
+                                    mintermichar[1][nivo][mintermadd][m] = buffer[m];
+                                }
+                            }
+                        }
+
+                        mintermadd++;    
+                        end = false;
                     }
+                    praskoci:;
                 }
                 preskoci:;
             }
         }
+
+        
+        if(end)
+        {
+            goto finish;
+        } 
+        for(int i = 0;i < numSpremenljivke+1;i++)
+        {
+            for(int j = 0;j < numMintermi;j++)
+            {
+                for(int k = 0;k < numSpremenljivke;k++)
+                {
+                    std::cout << mintermichar[1][i][j][k] << ' ';
+                    mintermichar[0][i][j][k] = mintermichar[1][i][j][k];
+                    mintermichar[1][i][j][k] = '.';
+                }
+            
+                std::cout << std::endl;
+            }
+            std::cout << "\n" << std::endl;
+        }
+        std::cout << "_____________________\n";  
     }
+    finish:;
+
+    //printanje rezultatov
+
 
     return 0;
 
